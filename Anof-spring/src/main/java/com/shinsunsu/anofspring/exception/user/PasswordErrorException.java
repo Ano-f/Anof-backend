7ac92fcf45291fe217e6474635656f7dec8a27c7
JoSunHee <82442788+JoSunHee@users.noreply.github.com>
2022-05-19 02:11:28 +0900

    [FEAT] 로그인 기능 추가
    Id/PW 오류시 예외 페이지로 이동X -> 수정 필요

--- Anof-spring/src/main/java/com/shinsunsu/anofspring/exception/user/PasswordErrorException.java
+++ Anof-spring/src/main/java/com/shinsunsu/anofspring/exception/user/PasswordErrorException.java
@@ -1,11 +1,5 @@
 package com.shinsunsu.anofspring.exception.user;
 
 public class PasswordErrorException extends IllegalArgumentException {
-    public PasswordErrorException() {
-        super();
-    }
-
-    public PasswordErrorException(String msg) {
-        super(msg);
-    }
+    public PasswordErrorException(String msg) { super(msg); }
 }
