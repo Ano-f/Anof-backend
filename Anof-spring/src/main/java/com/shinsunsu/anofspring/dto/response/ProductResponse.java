97123b9c8d10eead7c193b13923d5d54de96f21d
JoSunHee <82442788+JoSunHee@users.noreply.github.com>
2022-06-29 03:24:46 +0900

    [REFACTOR] 키워드로 상품 검색 리팩토링

--- Anof-spring/src/main/java/com/shinsunsu/anofspring/dto/response/ProductResponse.java
+++ Anof-spring/src/main/java/com/shinsunsu/anofspring/dto/response/ProductResponse.java
@@ -1,4 +1,21 @@
 package com.shinsunsu.anofspring.dto.response;
 
+import com.shinsunsu.anofspring.domain.Product;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class ProductResponse {
+    String name;
+    String brand;
+    String image;
+
+    public static ProductResponse productResponse(Product product) {
+        return ProductResponse.builder()
+                .name(product.getName())
+                .brand(product.getBrand())
+                .image(product.getImage())
+                .build();
+    }
 }
