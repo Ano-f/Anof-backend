15532f2c1fe61de1ddb2cad6fd2e6790a3b9b63c
Yeaji Shin <akdma1227@gmail.com>
2022-10-27 00:11:27 +0900

    [MOD] 식품 추천

--- Anof_flask/app.py
+++ Anof_flask/app.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from sqlalchemy import create_engine
-import time
+import numpy as np
 
 app = Flask(__name__)
 
@@ -28,36 +28,35 @@ def recommend():
     user.drop(['allergyId', 'IngredientId', 'id_y', 'id'], axis=1, inplace=True)
     user.set_index('id_x', inplace=True)
 
-    corrMatrix_wo_std = pd.DataFrame(cosine_similarity(user), index=user.index, columns=user.index)
+    corrMatrix_wo_std = pd.DataFrame(cosine_similarity(user, user), index=user.index, columns=user.index)
 
     def get_similarUsers(userId):
         similar_score = corrMatrix_wo_std[userId]
-        #앞서 보정된 값을 가지고 내림차순으로 정렬
         similar_score = similar_score.sort_values(ascending=False)[:6]
         similar_users = pd.DataFrame(similar_score)
         
         if (userId in similar_users.index): 
             similar_users.drop(similar_users[similar_users.index==userId], inplace=True)
             
-        for i in range(len(similar_users)):
-            similarity = similar_users.iat[i,0]
-            if(similarity==0):
-                similar_users.drop(similar_users.index[i], inplace=True)
+        similar_users = similar_users.xs(userId, axis=1)  
             
-        print(similar_users)
+        for i in similar_users.index:
+            similarity = similar_users.at[i]
+            if(similarity==0):
+                similar_users.drop(index=i, inplace=True)
         return similar_users
     
     def get_likeproductBySimilarUsers():
-        products = []
-        products_append = products.append
+        
+        products = pd.DataFrame()
         
         for i in similar_users.index:   #유사한 사용자들의 선호 식품을 기반으로 추천 식품 choice
-            if (likeProduct['userId']==i).any(): #userId==i가 존재하는 경우 
+            if (likeProduct['userId']==i).any(): 
                 data = likeProduct.loc[likeProduct.userId==i]
                 data = data.pivot_table('isSelect', index='userId', columns='productId')
-                print(data)
-                products_append(data.loc[i])
-            else: similar_users.drop(index=i, inplace=True)
+                dataframe = pd.DataFrame(data)
+                dataframe = dataframe*similar_users.at[i]
+                products = pd.concat([products, dataframe])
         
         return products
     
@@ -75,30 +74,18 @@ def recommend():
     
     products = get_likeproductBySimilarUsers()
     
-    if len(products)==0: #유사 사용자의 선호 식품이 없는 경우
+    if products.empty: #유사 사용자의 선호 식품이 없는 경우
         response = "0"
         return response
-            
-    arr = []
-    arr_append = arr.append
-                
-    matrix = pd.DataFrame(data = products)
-    matrix.fillna(0, inplace=True)
-
-    for col in range(matrix.shape[1]):
-        recommendation = 0
-        for row in range(matrix.shape[0]):
-            recommendation += matrix.iat[row, col]*similar_users.iat[row, 0]
-        arr_append(recommendation)
-              
-    recommendProduct = pd.DataFrame(data=arr, index=matrix.columns, columns={'recommend'})
-
-    recommendProduct = recommendProduct.sort_values(by='recommend', ascending=False)[:10]
-    print(recommendProduct)
-                
-    recommendProduct.drop(recommendProduct[recommendProduct['recommend']==0].index, inplace=True)
-        
-    response = ','.join(str(n) for n in recommendProduct.index.values.tolist())
+         
+    products.fillna(0, inplace=True)
+    
+    products = pd.DataFrame(products.sum())
+    products.drop(products[products[0]==0].index, inplace=True)
+    products.sort_values(by=0, ascending=False)[:10]
+    
+    response = ','.join(str(n) for n in products.index.values.tolist())
+    
     return response
     
 if __name__ == '__main__':
